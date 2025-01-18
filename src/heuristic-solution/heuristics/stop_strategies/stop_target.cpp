d596719705676de7db311939772add673f71ede0
Hélder Lima <orfeowrk@gmail.com>
2025-01-18 16:55:52 -0300

    limit ttt execution in 30 seconds

--- src/heuristic-solution/heuristics/stop_strategies/stop_target.cpp
+++ src/heuristic-solution/heuristics/stop_strategies/stop_target.cpp
@@ -3,12 +3,21 @@
 #include <iostream>
 
 void StopByTargetStrategy::init() {
+  clock_t t1 = clock();
+  this->t1 = t1;
+  elapsedTime = 0.0f;
   currentValue = 0;
 }
 
-bool StopByTargetStrategy::stopCondition() { return target <= currentValue; }
+bool StopByTargetStrategy::stopCondition() {
+  return target <= currentValue || elapsedTime >= 30;
+}
 
-void StopByTargetStrategy::update(int value) { currentValue = value; }
+void StopByTargetStrategy::update(int value) { 
+  currentValue = value; 
+  clock_t t2 = clock();
+  elapsedTime = (t2 - t1) / (double)CLOCKS_PER_SEC;
+}
 
 void StopByTargetStrategy::print() const {
   std::cout << "StopByTargetStrategy\n";
--- src/heuristic-solution/heuristics/stop_strategies/stop_target.h
+++ src/heuristic-solution/heuristics/stop_strategies/stop_target.h
@@ -2,13 +2,16 @@
 #define __stop_target_h__
 
 #include "stop_strategy.h"
+#include <ctime>
 
 class StopByTargetStrategy : public StopStrategy {
   const int target;
   int currentValue;
+  double elapsedTime;
+  clock_t t1;
 
 public:
-  StopByTargetStrategy(int target) : target(target) {}
+  StopByTargetStrategy(int target) : target(target) { elapsedTime = 0; }
 
   virtual void init() override;
   virtual bool stopCondition() override;
