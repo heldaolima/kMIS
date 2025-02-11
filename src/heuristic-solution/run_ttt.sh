2ae1e52a5a364990dc9e2b2291d70aa6246cceb5
Hélder Lima <orfeowrk@gmail.com>
2025-02-11 09:44:35 -0300

    change ttt execution order

--- src/heuristic-solution/run_ttt.sh
+++ src/heuristic-solution/run_ttt.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 make
-./main.out -t -d --swap2 complete -a simple -p simple # VND is inefficient
-./main.out -t -d --swap2 no -p reactive # Reactive Perturb is inefficient
-./main.out -t --swap2 no  # ILS without \mu is inefficient
 ./main.out -t -d --swap2 no # Main ILS beats them
+./main.out -t --swap2 no  # ILS without \mu is inefficient
+./main.out -t -d --swap2 no -p reactive # Reactive Perturb is inefficient
+./main.out -t -d --swap2 complete -a simple -p simple # VND is inefficient
